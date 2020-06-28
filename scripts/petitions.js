7d657d3089e6193bb84e2f915c4b0bd37bfca972
luis <luic.sistem@gmail.com>
2020-06-28 09:05:22 -0500

    cambiando el api

--- scripts/petitions.js
+++ scripts/petitions.js
@@ -1,7 +1,21 @@
 
-const API_URL = ("https://my-json-server.typicode.com/luicsistem/tablero-scrum")
+const API_URL = ("https://my-json-server.typicode.com/luicsistem/tablero-scrum/tasks")
+
+
 
 axios.get(API_URL)
-.then(resp => console.log(resp))
-.catch(err => console.log(err)
-)
+.then(resp => fillTasks(resp.data))
+.catch(err => console.log(err))
+
+function fillTasks(data) {
+    data.map(d => {
+        let newTask = document.createElement("article");
+        let tasktitle = document.createElement("h3");
+        tasktitle.innerText = d.title
+
+        newTask.appendChild(tasktitle)
+
+        let column = document.getElementById("doneTasks")
+        column.appendChild(newTask)
+    })
+}
