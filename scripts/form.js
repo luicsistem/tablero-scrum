95178ff1d8cefd7327099b79b40b8d647e46c655
luis <luic.sistem@gmail.com>
2020-06-29 10:14:45 -0500

    agregando el metodo post

--- scripts/form.js
+++ scripts/form.js
@@ -1,4 +1,7 @@
 const form = document.getElementById("formTask")
+
+
+
 form.addEventListener("submit", ev => {
   ev.preventDefault()
   const formData = ev.target
@@ -12,7 +15,33 @@ form.addEventListener("submit", ev => {
     created: Number(moment().add(Number(days), "days").format("X")),
     state: "to-do"
   }
+  function createTasks (data ){
+    let newTask = document.createElement("article");
+    newTask.classList.add("task")
 
+
+    let tasktitle = document.createElement("h5");
+    tasktitle.innerText = data.title
+    newTask.appendChild(tasktitle)
+   
+
+
+
+    let taskPerson = document.createElement("p")
+    taskPerson.innerHTML = `<span> Responsable: </span> ${data.person}`
+    newTask.appendChild(taskPerson)
+    
+
+    let taskDeadline = document.createElement("p")
+    taskDeadline.innerHTML = `<span>Plazo: </span> ${unixToDate(data.deadline)}`
+    newTask.appendChild(taskDeadline)
+
+    let columnTodo = document.getElementById("todoTasks")
+    columnTodo.appendChild(newTask)
+    newTask.classList.add("postit")
+
+  }
+ 
   axios.post(API_URL, data)
   .then(resp => {
     createTasks(resp.data)
